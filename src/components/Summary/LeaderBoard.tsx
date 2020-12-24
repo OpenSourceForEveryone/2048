da89e0e806bc31c071b42ef7af929d372236af3f
AbhiNatak <63898312+AbhiNatak@users.noreply.github.com>
2020-12-24 14:08:54 +0530

    PR For 2048 code review  (#3)
    * 2048 Initial Commit
    
    * Bug Fixes: Iteration-1
    
    * Bug fixes : Iteration 1
    
    * Bug Fixes: Iteration 02
    
    * Game Board Fixes
    
    * Board fixes for Desktop and Android
    
    * Addressing 2048 feedbacks and Minor tweaks
    
    * Addressing PR and Code clean up
    
    * linting whole module
    
    * Fixing Config issue
    
    * Fix for Localization
    
    * cleaning up the strings
    
    * version update
    
    * Minor string changes
    
    * string changes
    
    * Adding new entries in localozation
    
    * Adding new entries in localozation
    
    * Addressing bug bash issue
    
    * Code Cleanup and Creationview fixes
    
    * 2048 code
    
    * config commit
    
    * local update summay mutator

---
+++ src/components/Summary/LeaderBoard.tsx
@@ -0,0 +1,76 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { observer } from "mobx-react";
+import * as React from "react";
+import getStore, { LeaderBoard } from "../../store/SummaryStore";
+import "./Summary.scss";
+import { List } from "@fluentui/react-northstar";
+import { Localizer } from "../../utils/Localizer";
+import { Constants } from "../../utils/Constants";
+
+/**
+ * <LeaderBoardView> component for Leaderboard on summary page
+ * @observer decorator on the component this is what tells MobX to rerender the component whenever the data it relies on changes.
+ */
+@observer
+export class LeaderBoardView extends React.PureComponent<any, any> {
+    private scores: LeaderBoard[];
+    constructor(props) {
+        super(props);
+        this.scores = getStore().leaderBoard;
+        this.state = {
+            visible: Constants.DEFAULT_NUMBER_OF_RECORD
+        };
+        this.showMore = this.showMore.bind(this);
+    }
+    render() {
+
+        // preparing the leader baord
+        return (
+            <>
+                {this.scores && this.scores.length ?
+                    <>
+                        <List
+                            items={this.getListItems().slice(0, this.state.visible)}
+                            aria-label="staticHeadlessTable"
+                            className="table-container" />
+                        {
+                            this.scores.length > Constants.DEFAULT_NUMBER_OF_RECORD && this.scores.length > this.state.visible ?
+                                <span className="link leaderboard-link" onClick={this.showMore}>+ {Localizer.getString("LoadMore")}</span>
+                                :
+                                <div> </div>
+                        }
+                    </> :
+                    <div className="content">
+                        <label>
+                            {this.props.noOneHasResponded}
+                        </label>
+                    </div>
+                }
+            </>
+        );
+    }
+
+    // Helper method to increase the row visibility count
+    private showMore() {
+        this.setState((prev) => {
+            return { visible: prev.visible + Constants.RECORD_INCREMENT_FACTOR };
+        });
+    }
+
+    // Helper method to get the list items
+    private getListItems(): any[] {
+        let items = [];
+        if (this.scores && this.scores.length > 0) {
+            this.scores.forEach(element => {
+                items.push({
+                    key: element.playerId,
+                    header: element.playerName,
+                    headerMedia: <strong>{element.score}</strong>
+                });
+            });
+        }
+        return items || [];
+    }
+}
---
+++ src/mutator/SummaryMutator.ts
@@ -0,0 +1,158 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { mutator } from "satcheljs";
+import getStore, { LeaderBoard } from "./../store/SummaryStore";
+import {
+    setProgressStatus,
+    setContext,
+    setDueDate,
+    setGameTitle,
+    showMoreOptions,
+    setActionInstance,
+    fetchMyScore,
+    fetchLeaderBoard,
+    setGameStatus,
+    setLeaderboardVisibilityFlag,
+    setIsActionDeleted,
+    gameCloseAlertOpen,
+    gameExpiryChangeAlertOpen,
+    gameDeleteAlertOpen,
+    updateActionInstance
+} from "./../actions/SummaryActions";
+import * as actionSDK from "@microsoft/m365-action-sdk";
+import { UxUtils } from "../utils/UxUtils";
+
+/**
+ * Summary view mutators to modify store data on which summmary view relies
+ */
+
+mutator(setProgressStatus, (msg) => {
+    const store = getStore();
+    store.progressStatus = {
+        ...getStore().progressStatus,
+        ...msg.status,
+    };
+});
+
+mutator(setContext, (msg) => {
+    const store = getStore();
+    store.context = msg.context;
+    store.local = msg.context.locale;
+});
+
+mutator(setActionInstance, (msg) => {
+    const store = getStore();
+    store.actionInstance = msg.actionInstance;
+});
+
+mutator(setDueDate, (msg) => {
+    const store = getStore();
+    store.dueDate = msg.date;
+});
+
+mutator(setGameTitle, (msg) => {
+    const store = getStore();
+    store.title = msg.title;
+});
+
+mutator(setGameStatus, (msg) => {
+    const store = getStore();
+    store.isGameExpired = msg.status === actionSDK.ActionStatus.Active ? false : true;
+});
+
+mutator(setLeaderboardVisibilityFlag, () => {
+    const store = getStore();
+    if (store.context && store.actionInstance) {
+        const creatorId = store.actionInstance.creatorId;
+        const currentUserId = store.context.userId;
+        const datarowVisibility = store.actionInstance.dataTables[0].rowsVisibility;
+        if (creatorId === currentUserId || datarowVisibility === actionSDK.Visibility.All) {
+            getStore().isLeaderBoardVisible = true;
+        } else {
+            getStore().isLeaderBoardVisible = false;
+        }
+    }
+});
+
+mutator(fetchMyScore, (msg) => {
+
+    let rows: actionSDK.ActionDataRow[] = msg.myScore;
+    const options: Intl.DateTimeFormatOptions = {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+    };
+    if (rows && rows.length > 0) {
+        rows.forEach(element => {
+            getStore().scoreBoard.push(
+                {
+                    score: element.columnValues["2"],
+                    timeStamp: UxUtils.formatDate(new Date(element.createTime),
+                    getStore().actionInstance.customProperties[0].value,
+                    options)
+                }
+            );
+        });
+    }
+});
+
+mutator(fetchLeaderBoard, (msg) => {
+    let rows: actionSDK.ActionDataRow[] = msg.scores;
+    let newRows: LeaderBoard[] = [];
+    if (rows && rows.length > 0) {
+        rows.forEach(element => {
+            const player = newRows.find(p => p.playerId === element.creatorId);
+            if (player) {
+                if (Number(element.columnValues["2"]) > Number(player.score)) {
+                    newRows.find(p => p.playerId === element.creatorId).score = element.columnValues["2"];
+                }
+            } else {
+                newRows.push(
+                    {
+                        playerId: element.creatorId,
+                        playerName: element.columnValues["1"],
+                        score: element.columnValues["2"],
+                    }
+                );
+            }
+        });
+
+        getStore().leaderBoard = newRows.sort(function (a, b) {
+            return Number(b.score) - Number(a.score);
+        });
+    }
+});
+
+mutator(showMoreOptions, (msg) => {
+    const store = getStore();
+    store.showMoreOptionsList = msg.showMoreOptions;
+});
+
+mutator(updateActionInstance, (msg) => {
+    const store = getStore();
+    store.actionInstance = msg.actionInstance;
+    store.dueDate = msg.actionInstance.expiryTime;
+});
+
+mutator(gameCloseAlertOpen, (msg) => {
+    const store = getStore();
+    store.isGameCloseBoxOpen = msg.open;
+});
+
+mutator(gameExpiryChangeAlertOpen, (msg) => {
+    const store = getStore();
+    store.isChangeExpiryBoxOpen = msg.open;
+});
+
+mutator(gameDeleteAlertOpen, (msg) => {
+    const store = getStore();
+    store.isDeleteGameBoxOpen = msg.open;
+});
+
+mutator(setIsActionDeleted, (msg) => {
+    const store = getStore();
+    store.isActionDeleted = msg.isActionDeleted;
+});
